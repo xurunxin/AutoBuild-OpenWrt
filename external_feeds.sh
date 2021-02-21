4f85a559a66eee87824034f69d93615026969fa2
AltShift <xurx@live.com>
2021-02-22 04:45:52 +0800

    Update external_feeds.sh

--- external_feeds.sh
+++ external_feeds.sh
@@ -1,4 +1,4 @@
-cd package/lean/
+cd openwrt/package/lean/
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 # git clone https://github.com/kongfl888/luci-app-r2sflasher.git
 # git clone https://github.com/pymumu/smartdns.git
